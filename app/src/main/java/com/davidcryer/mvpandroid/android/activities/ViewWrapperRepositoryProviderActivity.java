ec05ad2a1b2d7831c03898bc35fdfc3f35d64406
davidcryer <davecryer.dc@gmail.com>
2017-01-28 17:20:17 +0000

    move view wrapper repository fragment class into view wrapper repository provider activity as static nested class

--- app/src/main/java/com/davidcryer/mvpandroid/android/activities/ViewWrapperRepositoryProviderActivity.java
+++ app/src/main/java/com/davidcryer/mvpandroid/android/activities/ViewWrapperRepositoryProviderActivity.java
@@ -2,11 +2,15 @@ package com.davidcryer.mvpandroid.android.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
 import com.davidcryer.mvpandroid.android.helpers.FragmentManagerHelper;
 import com.davidcryer.mvpandroid.android.presenter.repositories.ViewWrapperRepository;
+import com.davidcryer.mvpandroid.android.presenter.repositories.ViewWrapperRepositoryFactory;
+import com.davidcryer.mvpandroid.android.presenter.repositories.ViewWrapperRepositoryFactoryProvider;
 import com.davidcryer.mvpandroid.android.presenter.repositories.ViewWrapperRepositoryProvider;
+import com.davidcryer.mvpandroid.platformindependent.javahelpers.CastHelper;
 
 class ViewWrapperRepositoryProviderActivity extends AppCompatActivity implements ViewWrapperRepositoryProvider {
     private final static String FRAGMENT_TAG_VIEW_WRAPPER_REPOSITORY = "view wrapper repository";
@@ -29,5 +33,36 @@ class ViewWrapperRepositoryProviderActivity extends AppCompatActivity implements
     @Override
     public ViewWrapperRepository viewWrapperRepository() {
         return viewWrapperRepositoryFragment.viewWrapperRepository();
+    }
+
+    static class ViewWrapperRepositoryFragment extends Fragment {
+        private ViewWrapperRepository viewWrapperRepository;
+
+        public static ViewWrapperRepositoryFragment newInstance() {
+            return new ViewWrapperRepositoryFragment();
+        }
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setRetainInstance(true);
+            initialisePresenterRepository();
+        }
+
+        private void initialisePresenterRepository() {
+            viewWrapperRepository = viewWrapperRepositoryFactory().create();
+        }
+
+        private ViewWrapperRepositoryFactory viewWrapperRepositoryFactory() {
+            return viewWrapperRepositoryFactoryProvider().viewWrapperRepositoryFactory();
+        }
+
+        private ViewWrapperRepositoryFactoryProvider viewWrapperRepositoryFactoryProvider() {
+            return CastHelper.riskyCastToInterface(getActivity().getApplication(), ViewWrapperRepositoryFactoryProvider.class);
+        }
+
+        ViewWrapperRepository viewWrapperRepository() {
+            return viewWrapperRepository;
+        }
     }
 }
